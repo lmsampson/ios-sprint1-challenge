161ab3a72df4e4d4b62017415878ef9ef9474a58
Lisa Sampson <lisasampson0918@gmail.com>
2019-04-26 11:42:09 -0400

    Add dummy files and set up basic UI

---
+++ Movie List/Model Controller/MovieController.swift
@@ -0,0 +1,9 @@
+//
+//  MovieController.swift
+//  Movie List
+//
+//  Created by Lisa Sampson on 4/26/19.
+//  Copyright © 2019 Lambda School. All rights reserved.
+//
+
+import Foundation
---
+++ Movie List/Model/Movie.swift
@@ -0,0 +1,9 @@
+//
+//  Movie.swift
+//  Movie List
+//
+//  Created by Lisa Sampson on 4/26/19.
+//  Copyright © 2019 Lambda School. All rights reserved.
+//
+
+import Foundation
